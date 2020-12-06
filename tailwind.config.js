e9989ce92c1347725bd631f5058b5062403290fc
Šimon Bařák <barak.simon.as@gmail.com>
2020-12-06 23:03:37 +0100

    fix mobile font

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
           h1: {
             fontFamily: ["Inter", "sans-serif"],
             fontWeight: 400,
-            fontSize: "2.75rem",
+            fontSize: "2.5rem",
           },
           h2: {
             fontFamily: ["Inter", "sans-serif"],
